f368b1a796553cfa716f6d2110305f94eaba6760
Adi <adithiyans123@gmail.com>
2023-02-03 10:22:23 +0530

    Created basic arithmetic functions

---
+++ main.js
@@ -0,0 +1,28 @@
+function add(a,b){
+    let sum=0;
+    sum = a+b;
+    return sum;
+}
+
+function sub(a,b){
+    let dif=0;
+    dif = a-b;
+    return dif;
+}
+
+
+function mul(a,b){
+    let pro=0;
+    pro = a*b;
+    return pro;
+}
+
+
+function div(a,b){
+    let quo=0;
+    quo = a/b;
+    return quo;
+}
+
+
+console.log(div(3,2));
